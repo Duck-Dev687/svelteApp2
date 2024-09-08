1af091d71b2911a8c9954af724dc2524f77619cb
Duck Dev <duckdev32@gmail.com>
2024-09-08 19:40:41 +0300

    initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  purge: ['./src/**/*.svelte', './src/**/*.css'],
+	darkMode: false,
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
